f4033aa3710678e3c35876f3e4e472c645cf8117
shyang <shyang@humaxparcs.com>
2022-08-07 19:17:42 +0900

    add: carSharing Entity  추가

--- carsharing/src/main/java/org/example/model/sharingCar.java
+++ carsharing/src/main/java/org/example/model/sharingCar.java
@@ -1,5 +1,6 @@
 package org.example.model;
 
+import java.time.LocalDateTime;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -14,6 +15,7 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.Comment;
 
 @Getter
 @AllArgsConstructor
@@ -25,9 +27,25 @@ public class sharingCar extends BaseEntity{
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    @OneToOne
+    @ManyToOne
     @JoinColumn(name="sharing_sopt_id")
     private SharingSpot sharingSpot;
+    @Comment("차량번호")
+    private String carNumber;
+    @Comment("차량 위도")
+    private Double carLat;
+    @Comment("차량 경도")
+    private Double carLon;
+    @Comment("마지막 세차일")
+    private LocalDateTime lastWash;
+    @Comment("마지막 점검일")
+    private LocalDateTime lastRepair;
+    @Comment("주유 잔량")
+    private String fuelRate;
+    @Comment("차량이미지")
+    private String carImgPath;
+    @Comment("차량 소속 아이디")
+    private Integer CorpId;
 
 
 
